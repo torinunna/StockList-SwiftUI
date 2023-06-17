8444c21ac659394586388a7116033781350e97fa
torinunna <torinunna9588@gmail.com>
2023-06-17 12:28:01 +0900

    Initial Commit

---
+++ StockList-SwiftUI/StockList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockList_SwiftUIApp.swift
+//  StockList-SwiftUI
+//
+//  Created by YUJIN KWON on 2023/06/17.
+//
+
+import SwiftUI
+
+@main
+struct StockList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
